6f7f30d3f2611d3713ca8cef0eaafdb045bdd57c
jeffersonLucass <jeffersonlucas810@gmail.com>
2025-03-18 15:15:57 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,7 +14,7 @@ public class Program {
 		Department obj  = new Department(1, "Books");
 		//LocalDate.now() pega a data atual do sistema. ou LocalDate.of(ano, mes, dia) que é uma data específica. 
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.of(2000,12,8), 3000.0, obj);
-		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(obj);
 		System.out.println(seller);
