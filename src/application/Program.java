3eebec570ee2c1aa7433ba1df4a178143a0e5102
jeffersonLucass <jeffersonlucas810@gmail.com>
2025-03-18 15:01:21 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,21 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj  = new Department(1, "Books");
-		System.out.println(obj);
+		//LocalDate.now() pega a data atual do sistema. ou LocalDate.of(ano, mes, dia) que é uma data específica. 
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.of(2000,12,8), 3000.0, obj);
 		
+		
+		System.out.println(obj);
+		System.out.println(seller);
 		
 		
 		
