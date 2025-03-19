5eca617d1f5d7ca4f0b79de6ddb328c20d8b9f77
jeffersonLucass <jeffersonlucas810@gmail.com>
2025-03-19 14:45:21 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,16 +1,20 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
+// // A classe DaoFactory é responsável por criar instâncias de objetos DAO (Data Access Object).
+// Utiliza o padrão de projeto Factory para encapsular a lógica de criação dos DAOs.
+// Isso permite que o código que utiliza os DAOs não precise conhecer os detalhes de implementação,
+// promovendo a separação de responsabilidades e facilitando a manutenção e evolução do código.
+
 public class DaoFactory {
-	
-	
-	
-	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
-	}
-	
-	public static DepartmentDao createDepartmentDao() {
-		return new DepartmentDaoJDBC();
-	}
+
+    // Método que retorna uma instância de SellerDao.
+    public static SellerDao createSellerDao() {
+        return new SellerDaoJDBC(DB.getConnection());
+    }
+
+   
 }
+
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import model.entities.Seller;
 
 public interface SellerDao {
-
+ 
 	void insert(Seller obj);// inserir no banco de dados
 	void update(Seller obj);// atualizar no banco de dados
 	void deleteById(Integer id);// deletar no banco de dados
